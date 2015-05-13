538a8073713b2024eb6ec872d90491c5da67ae0d
Kyle Minshall <kyleminshall@gmail.com>
2015-05-12 19:28:49 -0700

    Adding problem 2

---
+++ problems/p2/README.md
@@ -0,0 +1,4 @@
+# Addition
+## Instructions
+1. Write a program with three different functions that add the numbers from 1 to 100.
+2. First method should be using a `for` loop. Second method should be using a `while` loop. Third method should be using recursion.
---
+++ problems/p2/addition.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+void methodFor()
+{
+
+}
+
+void methodWhile()
+{
+}
+
+void methodRecurse()
+{
+}
+
+
+int main(int argc, char ** argv)
+{
+    methodFor();
+    methodWhile();
+    methodRecurse();
+    return 0;
+}
