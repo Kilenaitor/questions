450cfd0a44d5b8fcecf43152c32f05cfa07ae138
Kyle Minshall <kyleminshall@gmail.com>
2015-05-22 14:26:19 -0700

    Adding (void)s

--- problems/p2/addition.h
+++ problems/p2/addition.h
@@ -1,14 +1,17 @@
 static int methodFor(int n)
 {
+	(void)n;
 	return 0;
 }
 
 static int methodWhile(int n)
 {
+	(void)n;
 	return 0;
 }
 
 static int methodRecurse(int n)
 {
+	(void)n;
 	return 0;
 }
